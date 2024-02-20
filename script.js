4212819d7934115ae4d4f5531665c5a66a5f2704
mcl8991 <michaellupiano@yahoo.com>
2024-02-20 00:14:59 -0500

    a3

---
+++ script.js
@@ -0,0 +1,12 @@
+ var libButton = document.getElementById('lib-button');
+        var libIt = function() {
+            var storyDiv = document.getElementById("story");
+            var adverb = document.getElementById("adverb")
+            var adjective = document.getElementById("adjective")
+            var food = document.getElementById("food")
+            var price = document.getElementById("price")
+            var emotion = document.getElementById("emotion")
+            
+            storyDiv.innerHTML = "You walk up to the counter and  "+ adverb.value + " order a pizza with extra  " + adjective.value + " " + food.value + ". The price of your pizza came out to be "+ price.value + " dollars. You are " + emotion.value + " about your order." ;
+        };
+        libButton.addEventListener('click', libIt);
